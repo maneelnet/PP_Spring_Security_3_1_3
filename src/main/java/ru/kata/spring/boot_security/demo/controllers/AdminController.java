c80864041a9942228281b4878a9440428c44a441
maneelnet <maneelnet@gmail.com>
2023-01-04 13:13:48 +0900

    PP_Spring_Security_3_1_3

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,61 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private RoleService roleService;
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setRoleService(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/")
+    public String showUsers(Model model) {
+        model.addAttribute("users", userService.usersList());
+        return "admin";
+    }
+
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("roles", roleService.rolesList());
+        model.addAttribute("user", new User());
+        return "user_form";
+    }
+
+    @PostMapping("/save")
+    public String create(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin/";
+    }
+
+    @GetMapping("edit/{id}")
+    public String editUser(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("roles", roleService.rolesList());
+        model.addAttribute("user", userService.showUser(id));
+        return "user_form";
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    public String delete(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin/";
+    }
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImp.java
@@ -0,0 +1,25 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImp implements RoleService{
+
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public void setRoleRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<Role> rolesList() {
+        return roleRepository.findAll();
+    }
+
+}
